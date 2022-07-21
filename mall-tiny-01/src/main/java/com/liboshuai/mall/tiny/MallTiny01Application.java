b0ebefb1fcf36da2c4f2517d87e841552dcd31d9
liboshuai01 <liboshuai01@gmail.com>
2022-07-21 12:27:48 +0800

    增加xxl-job配置，启动swagger配置

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/MallTiny01Application.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/MallTiny01Application.java
@@ -7,12 +7,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Author: liboshuai
  * @Date: 2022-07-10 20:35
  * @Description: 请求返回码
  */
+@EnableSwagger2
 @SpringBootApplication
 @MapperScan("com.liboshuai.mall.tiny.mapper")
 public class MallTiny01Application {
