a0991f8a8bc041fa064496bc51559d1822fe05fe
liboshuai01 <liboshuai01@gmail.com>
2022-07-27 13:17:57 +0800

    添加UmsAdminController中“根据用户名获取权限信息”的接口

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/controller/UmsAdminController.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/controller/UmsAdminController.java
@@ -1,8 +1,17 @@
 package com.liboshuai.mall.tiny.controller;
 
 
+import com.liboshuai.mall.tiny.common.api.CommonResult;
+import com.liboshuai.mall.tiny.domain.dao.UmsPermission;
+import com.liboshuai.mall.tiny.service.UmsAdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,8 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author liboshuai
  * @since 2022-07-26
  */
+@Api(tags = "后台用户", value = "UmsAdminController")
 @RestController
 @RequestMapping("/mall.tiny/ums-admin")
 public class UmsAdminController {
+
+    @Autowired
+    private UmsAdminService umsAdminService;
+
+    @ApiOperation(value = "根据用户名获取权限信息", httpMethod = "GET")
+    @GetMapping("/findPermissions")
+    public CommonResult<List<UmsPermission>> findPermissions(Long adminId) {
+        List<UmsPermission> permissions = umsAdminService.findPermissions(adminId);
+        return CommonResult.success(permissions);
+    }
 
 }
