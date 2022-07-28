024851eab165a6b2a4d3e6876754afa3eb70da15
liboshuai01 <liboshuai01@gmail.com>
2022-07-28 17:59:52 +0800

    添加“获取验证码”、“判断验证码是否正确”功能

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/controller/UmsMemberController.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/controller/UmsMemberController.java
@@ -1,8 +1,12 @@
 package com.liboshuai.mall.tiny.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.liboshuai.mall.tiny.common.api.CommonResult;
+import com.liboshuai.mall.tiny.service.UmsMemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -12,8 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @author liboshuai
  * @since 2022-07-26
  */
+@Api(tags = "会员登录注册管理", value = "UmsMemberController")
 @RestController
 @RequestMapping("/mall.tiny/ums-member")
 public class UmsMemberController {
 
+    private final UmsMemberService umsMemberService;
+
+    @Autowired
+    public UmsMemberController(UmsMemberService umsMemberService) {
+        this.umsMemberService = umsMemberService;
+    }
+
+    @ApiOperation(value = "获取验证码", httpMethod = "GET")
+    @GetMapping(value = "/getAuthCode")
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return umsMemberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation(value = "判断验证码是否正确", httpMethod = "POST")
+    @PostMapping(value = "/verifyAuthCode")
+    public CommonResult verifyAuthCode(@RequestParam String telephone, @RequestParam String authCode) {
+        return umsMemberService.verifyAuthCode(telephone, authCode);
+    }
 }
--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/service/UmsMemberService.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/service/UmsMemberService.java
@@ -1,6 +1,7 @@
 package com.liboshuai.mall.tiny.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.liboshuai.mall.tiny.common.api.CommonResult;
 import com.liboshuai.mall.tiny.domain.dao.UmsMember;
 
 /**
@@ -13,4 +14,16 @@ import com.liboshuai.mall.tiny.domain.dao.UmsMember;
  */
 public interface UmsMemberService extends IService<UmsMember> {
 
+    /**
+     * 生成验证码
+     *
+     * @param telephone 手机号码
+     * @return 验证码信息
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
 }
