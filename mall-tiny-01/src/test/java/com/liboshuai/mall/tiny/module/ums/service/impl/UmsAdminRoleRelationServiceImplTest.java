b00c167396148b2d9fc0b305feb332281f9e4da5
liboshuai01 <liboshuai01@gmail.com>
2022-09-08 23:18:04 +0800

    修复数据库链接bug；测试“通过用户名称获取角色信息”service方法

---
+++ mall-tiny-01/src/test/java/com/liboshuai/mall/tiny/module/ums/service/impl/UmsAdminRoleRelationServiceImplTest.java
@@ -0,0 +1,8 @@
+package com.liboshuai.mall.tiny.module.ums.service.impl;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class UmsAdminRoleRelationServiceImplTest extends TestCase {
+
+}
---
+++ mall-tiny-01/src/test/java/com/liboshuai/mall/tiny/module/ums/service/impl/UmsRoleServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.liboshuai.mall.tiny.module.ums.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.liboshuai.mall.tiny.MallTiny01Application;
+import com.liboshuai.mall.tiny.module.ums.domain.dao.UmsRole;
+import com.liboshuai.mall.tiny.module.ums.service.UmsAdminRoleRelationService;
+import com.liboshuai.mall.tiny.module.ums.service.UmsAdminService;
+import com.liboshuai.mall.tiny.module.ums.service.UmsRoleService;
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MallTiny01Application.class)
+public class UmsRoleServiceImplTest extends TestCase {
+
+    @Autowired
+    private UmsAdminService umsAdminService;
+
+    @Autowired
+    private UmsAdminRoleRelationService umsAdminRoleRelationService;
+
+    @Autowired
+    private UmsRoleService umsRoleService;
+
+    @Test
+    public void testFindRolesByRoleIds() {
+        Long adminId = umsAdminService.findUserIdByUserName("admin");
+        List<Long> roleIds = umsAdminRoleRelationService.findRoleIdsByUserId(adminId);
+        List<UmsRole> roles = umsRoleService.findRolesByRoleIds(roleIds);
+        log.info("roles: {}", JSON.toJSONString(roles));
+    }
+}
