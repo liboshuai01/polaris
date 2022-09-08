6a13cc9159cefa60023ce6789b90e338bbc10fcf
liboshuai01 <liboshuai01@gmail.com>
2022-09-08 23:20:38 +0800

    删除“根据用户名称查询用户id”controller

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/ums/controller/UmsAdminController.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/ums/controller/UmsAdminController.java
@@ -1,24 +1,11 @@
 package com.liboshuai.mall.tiny.module.ums.controller;
 
 
-import com.liboshuai.mall.tiny.common.api.ResponseCode;
-import com.liboshuai.mall.tiny.common.api.ResponseResult;
-import com.liboshuai.mall.tiny.module.ums.domain.dao.UmsAdmin;
-import com.liboshuai.mall.tiny.module.ums.domain.dao.UmsPermission;
-import com.liboshuai.mall.tiny.module.ums.domain.dao.UmsRole;
-import com.liboshuai.mall.tiny.module.ums.domain.dto.UmsAdminLoginParam;
-import com.liboshuai.mall.tiny.module.ums.service.UmsAdminService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * <p>
@@ -33,16 +20,5 @@ import java.util.Objects;
 @RequestMapping("/ums/admin")
 @Slf4j
 public class UmsAdminController {
-
-    @Autowired
-    private UmsAdminService umsAdminService;
-
-
-    @ApiOperation(value = "根据会员名称查询用户id", httpMethod = "GET")
-    @GetMapping("/findUserIdByUserName")
-    public ResponseResult<Long> findUserIdByUserName(@RequestParam String userName) {
-        Long userId = umsAdminService.findUserIdByUserName(userName);
-        return ResponseResult.success(userId);
-    }
 
 }
