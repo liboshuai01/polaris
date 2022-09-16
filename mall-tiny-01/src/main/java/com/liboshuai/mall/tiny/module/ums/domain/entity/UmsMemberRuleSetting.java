96d0254dba090f3c99a07933e1589a132d4694d7
liboshuai01 <liboshuai01@gmail.com>
2022-09-16 14:56:35 +0800

    继承entity基类 2.1

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/ums/domain/entity/UmsMemberRuleSetting.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/ums/domain/entity/UmsMemberRuleSetting.java
@@ -52,27 +52,6 @@ public class UmsMemberRuleSetting extends BaseEntity {
     @TableField("type")
     private Integer type;
 
-    @ApiModelProperty("创建用户")
-    @TableField("create_user")
-    private String createUser;
-
-    @ApiModelProperty("创建时间")
-    @TableField("create_time")
-    private LocalDateTime createTime;
-
-    @ApiModelProperty("更新用户")
-    @TableField("update_user")
-    private String updateUser;
-
-    @ApiModelProperty("更新时间")
-    @TableField("update_time")
-    private LocalDateTime updateTime;
-
-    @ApiModelProperty("是否被逻辑删除 0-未被删除 1-已被删除")
-    @TableField("is_delete")
-    private String isDelete;
-
-
 }
 
 
