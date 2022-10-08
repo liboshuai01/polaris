bbb870d08548dfd65576b58ea09172ffd95e8d1e
liboshuai01 <liboshuai01@gmail.com>
2022-10-09 06:18:27 +0800

    代码报错修复；

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/ums/service/impl/UmsAdminServiceImpl.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/ums/service/impl/UmsAdminServiceImpl.java
@@ -6,7 +6,6 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.liboshuai.mall.tiny.module.ums.domain.entity.UmsAdmin;
 import com.liboshuai.mall.tiny.module.ums.domain.dto.UmsAdminDTO;
-import com.liboshuai.mall.tiny.module.ums.domain.mapStruct.UmsAdminConverter;
 import com.liboshuai.mall.tiny.module.ums.domain.vo.UmsAdminVO;
 import com.liboshuai.mall.tiny.module.ums.mapper.UmsAdminMapper;
 import com.liboshuai.mall.tiny.module.ums.service.UmsAdminService;
@@ -19,7 +18,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * <p>
@@ -32,8 +30,6 @@ import java.util.Objects;
 @Service
 public class UmsAdminServiceImpl extends ServiceImpl<UmsAdminMapper, UmsAdmin> implements UmsAdminService {
 
-    @Autowired
-    private UmsAdminConverter umsAdminConverter;
     @Autowired
     private UmsAdminMapper umsAdminMapper;
 
@@ -56,7 +52,9 @@ public class UmsAdminServiceImpl extends ServiceImpl<UmsAdminMapper, UmsAdmin> i
         LambdaQueryWrapper<UmsAdmin> umsMemberLambdaQueryWrapper = new LambdaQueryWrapper<>();
         umsMemberLambdaQueryWrapper.eq(UmsAdmin::getUsername, username);
         UmsAdmin umsAdmin = umsAdminMapper.selectOne(umsMemberLambdaQueryWrapper);
-        return umsAdminConverter.entityToDTO(umsAdmin);
+        UmsAdminDTO umsAdminDTO = new UmsAdminDTO();
+        BeanUtils.copyProperties(umsAdmin, umsAdminDTO);
+        return umsAdminDTO;
     }
 
     /**
