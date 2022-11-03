80f2c1ea5c8ae84fe3f5639fb5fcd047efa7bd9c
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2022-11-04 01:05:26 +0800

    现在idea本地启动Jasypt解密成功了,但是服务器上jar包启动还是失败的

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/MallTiny01Application.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/MallTiny01Application.java
@@ -17,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 @SpringBootApplication
+@EnableEncryptableProperties
 @MapperScan("com.liboshuai.mall.tiny.module.*.mapper")
 public class MallTiny01Application {
     public static void main(String[] args) {
