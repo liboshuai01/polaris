36375b81e964c870d8da19b7f10f87a8ffa0b798
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2022-11-04 23:48:41 +0800

    es自定义基本查询

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/nosql/elasticsearch/document/BookRepository.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/nosql/elasticsearch/document/BookRepository.java
@@ -2,10 +2,23 @@ package com.liboshuai.mall.tiny.nosql.elasticsearch.document;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 /**
  * @Author: liboshuai
  * @Date: 2022-11-02 21:47
  * @Description:
  */
 public interface BookRepository extends ElasticsearchRepository<Book, String> {
+
+    /**
+     * 根据作者查询
+     */
+    List<Book> findByAuthor(String author);
+
+    /**
+     * 根据内容查询
+     */
+    List<Book> findByContent(String content);
+
 }
