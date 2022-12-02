4a31e2a549baabab40830b5c3c75f18e76bb14ae
sun <sunjunhao01>
2022-12-02 22:51:58 +0800

    对象的创建及初始化

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop/encapsulation/Student.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop/encapsulation/Student.java
@@ -5,4 +5,15 @@ package com.liboshuai.mall.tiny.zlearning.Test.literal.oop.encapsulation;
  * @date02/12/202210:29 PM
  */
 public class Student {
+    private int age;
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        if (age >= 0 && age <= 200){
+            this.age = age;
+        }else {
+            System.out.println("年龄数据有问题，应该不是人的年龄");
+        }
+    }
 }
