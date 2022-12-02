026f90f26850d86466eb7d231f2e4330cf2bdc07
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2022-12-03 00:14:16 +0800

    数据库中导入所有商品到ES 1.2 添加controller

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/pms/controller/PmsProductController.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/module/pms/controller/PmsProductController.java
@@ -1,6 +1,12 @@
 package com.liboshuai.mall.tiny.module.pms.controller;
 
 
+import com.liboshuai.mall.tiny.compone.response.ResponseResult;
+import com.liboshuai.mall.tiny.module.pms.service.PmsProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mall.tiny.module.pms/pms-product")
 public class PmsProductController {
+
+    @Autowired
+    private PmsProductService pmsProductService;
+
+    @ApiOperation(value = "导入全部商品到es中", httpMethod = "POST")
+    @PostMapping("/importAllProductToEs")
+    public ResponseResult<Integer> importAllProductToEs() {
+        int result = pmsProductService.importAllProductToEs();
+        return ResponseResult.success(result);
+    }
 
 }
