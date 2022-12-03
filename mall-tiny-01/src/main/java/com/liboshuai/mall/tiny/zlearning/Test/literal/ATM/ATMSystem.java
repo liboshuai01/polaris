7db40b0355a8c6a9cf57e6d292d5d8e33c07e694
sun <sunjunhao01>
2022-12-03 11:20:15 +0800

    ATM登录操作实现

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/ATM/ATMSystem.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/ATM/ATMSystem.java
@@ -16,13 +16,79 @@ public class ATMSystem {
      * 用户的开户操作
      * */
     private static void login(ArrayList<Account> accounts, Scanner sc) {
+        System.out.println("===================系统登录操作========================");
+        if (accounts.size() == 0){
+            System.out.println("对不起，当前系统中，无任何账户，请先开户，再来登录~~");
+            return;
+        }
+        while(true){
+            System.out.println("请您输入登录卡号:");
+            String cardId = sc.next();
+
+            Account acc = getAccountByCardId(cardId,accounts);
+            if (acc != null){
+                while(true){
+                    System.out.println("请您输入登录密码：");
+                    String passWord = sc.next();
+                    if (acc.getPassWord().equals(passWord)){
+                        System.out.println("恭喜您，" + acc.getUserName() +"先生/女生进入系统，您的卡号是：" + acc.getCardId());
+                        showUserCommand(sc,acc,accounts);
+                        return;
+                    }else {
+                        System.out.println("对不起，您输入的密码有误~~");
+                    }
+                }
+            }else {
+                System.out.println("对不起，系统中不存在该账户卡号~~");
+            }
+        }
 
     }
     /**
      * 展示页
      * */
     private static void showUserCommand(Scanner sc, Account acc, ArrayList<Account> accounts) {
+        while (true){
+            System.out.println("===============用户操作页===================");
+            System.out.println("1、查询账户");
+            System.out.println("2、存款");
+            System.out.println("3、取款");
+            System.out.println("4、转账");
+            System.out.println("5、修改密码");
+            System.out.println("6、退出");
+            System.out.println("7、注销账户");
+            System.out.println("请选择：");
 
+            int command = sc.nextInt();
+            switch (command){
+                case 1:
+                    showAccount(acc);
+                    break;
+                case 2:
+                    depositMoney(acc,sc);
+                    break;
+                case 3:
+                    drawMoney(acc,sc);
+                    break;
+                case 4:
+                    transferMoney(sc,acc,accounts);
+                    break;
+                case 5:
+                    updatePassWord(sc,acc);
+                    return;
+                case 6:
+                    System.out.println("退出成功，欢迎下次光临");
+                    return;
+                case 7:
+                    if (deleteAccount(acc,sc,accounts)){
+                        return;
+                    }else {
+                        break;
+                    }
+                default:
+                    System.out.println("您输入的操作命令不正确");
+            }
+        }
     }
 
     /**
