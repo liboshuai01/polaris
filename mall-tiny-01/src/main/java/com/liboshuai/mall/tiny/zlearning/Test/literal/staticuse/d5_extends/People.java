2a3b3884d2d321a4eef2ca2de986b04821afa856
sun <sunjunhao01>
2022-12-04 13:32:38 +0800

    static

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/staticuse/d5_extends/People.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/staticuse/d5_extends/People.java
@@ -5,4 +5,7 @@ package com.liboshuai.mall.tiny.zlearning.Test.literal.staticuse.d5_extends;
  * @date04/12/20221:30 PM
  */
 public class People {
+    public void run(){
+        System.out.println("人会跑--");
+    }
 }
--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/staticuse/d5_extends/Student.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/staticuse/d5_extends/Student.java
@@ -4,5 +4,5 @@ package com.liboshuai.mall.tiny.zlearning.Test.literal.staticuse.d5_extends;
  * @author:Sun
  * @date04/12/20221:30 PM
  */
-public class Student {
+public class Student extends People{
 }
