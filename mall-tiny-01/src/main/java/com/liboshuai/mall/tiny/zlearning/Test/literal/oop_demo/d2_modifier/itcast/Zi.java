55c48a465aceab5548517e34b8bc73888c8885ea
sun <sunjunhao01>
2022-12-05 18:45:47 +0800

    java基础高级

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop_demo/d2_modifier/itcast/Zi.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop_demo/d2_modifier/itcast/Zi.java
@@ -1,8 +1,18 @@
 package com.liboshuai.mall.tiny.zlearning.Test.literal.oop_demo.d2_modifier.itcast;
 
+import com.liboshuai.mall.tiny.zlearning.Test.literal.oop_demo.d2_modifier.Fu;
+
 /**
  * @author:Sun
  * @date05/12/20226:34 PM
  */
-public class Zi {
+public class Zi extends Fu {
+    public static void main(String[] args) {
+        Zi zi = new Zi();
+        zi.protectedMethod();
+        zi.publicMethod();
+
+        Fu fu = new Fu();
+        fu.publicMethod();
+    }
 }
