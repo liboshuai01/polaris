697a8c787953e37abd44486b8b4ae569f659f419
sun <sunjunhao01>
2022-12-05 23:12:48 +0800

    多态

---
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop_app3/d4_polymorphic_test/Computer.java
@@ -0,0 +1,20 @@
+package com.liboshuai.mall.tiny.zlearning.Test.literal.oop_app3.d4_polymorphic_test;
+
+/**
+ * @author:Sun
+ * @date05/12/202210:59 PM
+ */
+public class Computer {
+    public void installUSB(USB u){
+        u.connect();
+        if ( u instanceof Mouse){
+            Mouse m = (Mouse) u;
+            m.click();
+        } else if (u instanceof  KeyBoard) {
+            KeyBoard k = (KeyBoard) u;
+            k.keyDown();
+        }
+
+        u.unconnect();
+    }
+}
