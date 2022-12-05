08427430c92ba44f41b366bc6e352e5c688bbf72
sun <sunjunhao01>
2022-12-05 22:26:04 +0800

    接口

---
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop_demo/d13_interface_jdk8/Test.java
@@ -0,0 +1,14 @@
+package com.liboshuai.mall.tiny.zlearning.Test.literal.oop_demo.d13_interface_jdk8;
+
+/**
+ * @author:Sun
+ * @date05/12/202210:20 PM
+ */
+public class Test {
+    public static void main(String[] args) {
+        PingPongMan p = new PingPongMan();
+        p.run();
+        SportMan.inAddr();
+
+    }
+}
