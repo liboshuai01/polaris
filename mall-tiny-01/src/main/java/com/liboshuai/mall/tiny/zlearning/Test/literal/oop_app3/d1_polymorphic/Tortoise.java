22c6e7dc0fef5ece78bb1b676da20b35b72f5bcd
sun <sunjunhao01>
2022-12-05 22:43:23 +0800

    多态

---
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop_app3/d1_polymorphic/Tortoise.java
@@ -0,0 +1,13 @@
+package com.liboshuai.mall.tiny.zlearning.Test.literal.oop_app3.d1_polymorphic;
+
+/**
+ * @author:Sun
+ * @date05/12/202210:38 PM
+ */
+public class Tortoise extends Animal{
+    public String name = "乌龟名称";
+    @Override
+    public void run(){
+        System.out.println("🐢跑的非常慢~~~");
+    }
+}
