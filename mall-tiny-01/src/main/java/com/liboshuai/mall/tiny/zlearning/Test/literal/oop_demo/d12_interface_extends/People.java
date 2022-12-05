25cfbcb0e14b0ddc3dc93e79088b2bfd77ba8100
sun <sunjunhao01>
2022-12-05 22:19:23 +0800

    接口

---
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/zlearning/Test/literal/oop_demo/d12_interface_extends/People.java
@@ -0,0 +1,10 @@
+package com.liboshuai.mall.tiny.zlearning.Test.literal.oop_demo.d12_interface_extends;
+
+/**
+ * @author:Sun
+ * @date05/12/202210:16 PM
+ */
+public interface People {
+    void eat();
+    void sleep();
+}
