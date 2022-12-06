3ba80bcfe4f8b1fd0d0d210f807699a73fdb25bd
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2022-12-07 00:41:14 +0800

    elasticsearch相关变更 0.5

--- mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/nosql/elasticsearch/repository/EsProductRepository.java
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/nosql/elasticsearch/repository/EsProductRepository.java
@@ -1,24 +1,24 @@
-package com.liboshuai.mall.tiny.nosql.elasticsearch.repository;
-
-import com.liboshuai.mall.tiny.nosql.elasticsearch.document.EsProduct;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-/**
- * @Author: liboshuai
- * @Date: 2022-12-02 16:20
- * @Description: es商品数据层
- */
-public interface EsProductRepository extends ElasticsearchRepository<EsProduct, Long> {
-
-    /**
-     * 根据商品名称,商品标题,商品关键字 分页查询
-     * @param name 商品名称
-     * @param subTitle 商品标题
-     * @param keywords 商品关键字
-     * @return 分页查询的结果
-     */
-    Page<EsProduct> findByNameOrSubTitleOrKeywords(String name, String subTitle, String keywords, Pageable pageable);
-
-}
+//package com.liboshuai.mall.tiny.nosql.elasticsearch.repository;
+//
+//import com.liboshuai.mall.tiny.nosql.elasticsearch.document.EsProduct;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//
+///**
+// * @Author: liboshuai
+// * @Date: 2022-12-02 16:20
+// * @Description: es商品数据层
+// */
+//public interface EsProductRepository extends ElasticsearchRepository<EsProduct, Long> {
+//
+//    /**
+//     * 根据商品名称,商品标题,商品关键字 分页查询
+//     * @param name 商品名称
+//     * @param subTitle 商品标题
+//     * @param keywords 商品关键字
+//     * @return 分页查询的结果
+//     */
+//    Page<EsProduct> findByNameOrSubTitleOrKeywords(String name, String subTitle, String keywords, Pageable pageable);
+//
+//}
