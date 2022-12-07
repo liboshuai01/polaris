eb5ec8d0f8635dcdfaf4c14a4807e9330fa96f7f
liboshuai01 <liboshuai01@gmail.com>
2022-12-07 15:00:00 +0800

    变更elasticSearch配置

---
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/compone/config/ElasticSearchConfig.java
@@ -0,0 +1,58 @@
+package com.liboshuai.mall.tiny.compone.config;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+
+/**
+ * @Auther: 李博帅
+ * @Date: 2022/12/6 21:03
+ * @Description: elasticsearch配置类
+ */
+@Configuration
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticSearch.host}")
+    private String elasticSearchHost;
+
+    @Value("${elasticSearch.port}")
+    private String elasticSearchPort;
+
+    @Value("${elasticSearch.scheme}")
+    private String elasticSearchScheme;
+
+    @Value("${elasticSearch.userName}")
+    private String elasticSearchUserName;
+
+    @Value("${elasticSearch.password}")
+    private String elasticSearchPassword;
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "123456"));
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"))
+                        .setHttpClientConfigCallback(httpClientBuilder -> {
+                            httpClientBuilder.disableAuthCaching();
+                            return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                        }));
+    }
+
+}
