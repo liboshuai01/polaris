e7f244b1ff50d5650d2f0c2d5cd9536329a40be9
liboshuai01 <liboshuai01@gmail.com>
2022-12-26 09:06:59 +0800

    取消订单消息的发送者

---
+++ mall-tiny-01/src/main/java/com/liboshuai/mall/tiny/compone/mq/CancelOrderSender.java
@@ -0,0 +1,16 @@
+package com.liboshuai.mall.tiny.compone.mq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: liboshuai
+ * @Date: 2022-12-26 09:05
+ * @Description: 取消订单消息的发送者
+ */
+@Slf4j
+@Component
+public class CancelOrderSender {
+
+}
