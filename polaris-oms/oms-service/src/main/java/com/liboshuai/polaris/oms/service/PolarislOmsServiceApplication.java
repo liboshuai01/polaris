2a001a8ad0abd710830ecd8f7bb8f7d277402048
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2023-01-01 05:05:36 +0800

    给oms pms sms添加openfeign注解，解决admin因为gateway监控失败的问题

--- polaris-oms/oms-service/src/main/java/com/liboshuai/polaris/oms/service/PolarislOmsServiceApplication.java
+++ polaris-oms/oms-service/src/main/java/com/liboshuai/polaris/oms/service/PolarislOmsServiceApplication.java
@@ -3,12 +3,14 @@ package com.liboshuai.polaris.oms.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Author: liboshuai
  * @Date: 2022-12-30 15:08
  * @Description: oms-servie启动类
  */
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PolarislOmsServiceApplication {
--- polaris-pms/pms-service/src/main/java/com/liboshuai/polaris/pms/service/PolarisPmsServiceApplication.java
+++ polaris-pms/pms-service/src/main/java/com/liboshuai/polaris/pms/service/PolarisPmsServiceApplication.java
@@ -3,12 +3,14 @@ package com.liboshuai.polaris.pms.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Auther: Bernardo
  * @Date: 2022/12/31 17:20
  * @Description:
  */
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PolarisPmsServiceApplication {
