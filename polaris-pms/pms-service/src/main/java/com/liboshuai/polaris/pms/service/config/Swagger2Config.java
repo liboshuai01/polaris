83099478e0c7770d4bed1e10371389914480574c
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2022-12-31 22:18:51 +0800

    添加商品模块pms

---
+++ polaris-pms/pms-service/src/main/java/com/liboshuai/polaris/pms/service/config/Swagger2Config.java
@@ -0,0 +1,33 @@
+package com.liboshuai.polaris.pms.service.config;
+
+import com.liboshuai.polaris.common.swagger.BaseSwaggerConfig;
+import com.liboshuai.polaris.common.swagger.SwaggerProperties;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2API文档的配置
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config extends BaseSwaggerConfig {
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.liboshuai.polaris")
+                .title("polaris商品模块")
+                .description("polaris商品模块相关接口文档")
+                .contactName("liboshuai")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+
+    @Bean
+    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return generateBeanPostProcessor();
+    }
+}
+
--- polaris-pms/pms-service/src/main/java/com/liboshuai/polaris/pms/service/one.java
+++
@@ -1,9 +0,0 @@
-package com.liboshuai.polaris.pms.service;
-
-/**
- * @Auther: Bernardo
- * @Date: 2022/12/31 13:13
- * @Description:
- */
-public class one {
-}
