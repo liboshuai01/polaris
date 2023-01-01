d92e927888d9d636f5af6be66fef16a2d13691a7
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2023-01-02 01:35:35 +0800

    gateway增加全局跨域配置

---
+++ polaris-security/security-service/src/main/java/com/liboshuai/polaris/security/config/GlobalCorsConfig.java
@@ -0,0 +1,29 @@
+package com.liboshuai.polaris.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Auther: Bernardo
+ * @Date: 2023/1/2 01:34
+ * @Description: 全局跨域配置
+ */
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
