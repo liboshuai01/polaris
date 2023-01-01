85385895cc0c27803a1d51e47c80227bce22b2ce
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2023-01-02 01:53:17 +0800

    变更/login接口请求方法为post

--- polaris-security/security-service/src/main/java/com/liboshuai/polaris/security/controller/LoginController.java
+++ polaris-security/security-service/src/main/java/com/liboshuai/polaris/security/controller/LoginController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @ApiOperation(value = "登录", httpMethod = "POST")
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseResult<?> login(@RequestBody LoginQuery loginQuery) {
         log.info("-------用户{}进行登录操作-------", JSONObject.toJSONString(loginQuery));
         return ResponseResult.success("登录成功");
