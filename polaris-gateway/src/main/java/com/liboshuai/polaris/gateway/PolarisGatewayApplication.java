322d8c22101240df545b4b74900434b8316e56ae
liboshuai01 <liboshuai01@gmail.com>
2023-02-03 13:57:40 +0800

    gateway集成sentinel熔断限流

--- polaris-gateway/src/main/java/com/liboshuai/polaris/gateway/PolarisGatewayApplication.java
+++ polaris-gateway/src/main/java/com/liboshuai/polaris/gateway/PolarisGatewayApplication.java
@@ -13,6 +13,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class PolarisGatewayApplication {
     public static void main(String[] args) {
+        // 让sentinel识别为网关服务
+        System.setProperty("csp.sentinel.app.type", "1");
         SpringApplication.run(PolarisGatewayApplication.class, args);
     }
 }
